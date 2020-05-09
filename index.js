f055457e2afcb7f173631ab8476ff282c5daffc0
Aude Rouaux <auderouaux@Audes-MacBook-Pro.local>
2020-05-09 13:49:29 +0200

    chore(index): set up server

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+// create a new Express Instance
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log('Server started and listening on port 3000!'))
